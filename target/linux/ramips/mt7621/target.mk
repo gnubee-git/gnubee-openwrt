464073dd0b87fa5ce7409448d34736c4f37f59c7
L. D. Pinney <ldpinney@gmail.com>
2016-06-08 12:09:12 -0500

    Minor change to /mt7621/target.mk

--- target/linux/ramips/mt7621/target.mk
+++ target/linux/ramips/mt7621/target.mk
@@ -4,10 +4,10 @@
 
 SUBTARGET:=mt7621
 BOARDNAME:=MT7621 based boards
-ARCH_PACKAGES:=ramips_1004kec
+ARCH_PACKAGES:=ramips_1004kc
 FEATURES+=usb
 CPU_TYPE:=1004kc
-CPU_SUBTYPE:=dsp
+
 DEFAULT_PACKAGES +=
 
 define Target/Description
